9cc83d00a51ada3209dd40389507197280e5d665
Mark Gabrielle Cay <markgabrieller@gmail.com>
2025-09-06 08:42:08 +0800

    refactor: remove unnecessary DateTime tests

--- tests/Unit/DateTimeTest.php
+++ tests/Unit/DateTimeTest.php
@@ -17,27 +17,4 @@ class DateTimeTest extends TestCase
             'Valid date should not produce DateTime errors'
         );
     }
-
-    public function testInvalidDateHasErrors()
-    {
-        $dt = \DateTime::createFromFormat('Y-m-d', '2025-02-30'); // Invalid Feb 30
-        $errors = \DateTime::getLastErrors();
-
-        // Should always produce errors regardless of PHP version
-        $this->assertTrue(
-            is_array($errors) && array_sum($errors) > 0,
-            'Invalid date should produce DateTime errors'
-        );
-    }
-
-    public function testValidDatetimeHasNoErrors()
-    {
-        $dt = \DateTime::createFromFormat('Y-m-d\TH:i', '2025-09-06T12:34');
-        $errors = \DateTime::getLastErrors();
-
-        $this->assertTrue(
-            $errors === false || (is_array($errors) && array_sum($errors) === 0),
-            'Valid datetime should not produce DateTime errors'
-        );
-    }
 }
